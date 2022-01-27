8f3d06725636d0eb653fa1587bb5cd981d248b08
prati <pratitibakshi0804@gmail.com>
2022-01-28 00:41:37 +0530

    Commit 2

---
+++ app/src/main/java/com/example/birthdayapp/Greeting.kt
@@ -0,0 +1,11 @@
+package com.example.birthdayapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Greeting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeting)
+    }
+}
--- app/src/main/java/com/example/birthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdayapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.birthdayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun createBirthdayCard(view: android.view.View) {
+     Toast.makeText(this,"Button was clicked!!",Toast.LENGTH_LONG).show()
+    }
+
+
 }
